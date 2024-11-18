9e6ff92bcf18e558886811069e80f181debf74e6
sripriya.srinivas <sripriya.srini04@gmail.com>
2024-11-18 18:12:01 +0530

    added new image

--- client/src/SelectState.js
+++ client/src/SelectState.js
@@ -49,17 +49,17 @@ const About = React.forwardRef((props, ref) => {
             <div>
                 <div style={{display: 'flex'}} data-aos="fade-up" data-aos-delay="300">
                     <div className='objective'>
-                        <img src="./1.png" alt="vision" height="50px"/>
+                        <img src="/1.png" alt="vision" height="50px"/>
                         <h3>Vision</h3>
                         <p>To improve the quality of service delivery to the citizen and the quality of work environment of the RTOs.</p>
                     </div>
                     <div className='objective' data-aos="fade-up" data-aos-delay="500">
-                        <img src="./2.png" alt="mission" height="50px"/>
+                        <img src="/2.png" alt="mission" height="50px"/>
                         <h3>Mission</h3>
                         <p>To automate all Vehicle Registration and Driving License related activities in transport authorities of country with introduction of smart card technology to handle issues like inter state transport vehicle movement and to create state and national level registers of vehicles/DL information</p>
                     </div>
                     <div className='objective' data-aos="fade-up" data-aos-delay="700">
-                         <img src="./3.png" alt="obj" height="50px"/>
+                         <img src="/3.png" alt="obj" height="50px"/>
                         <h3>Objectives</h3>
                             <p>Better services to Transport Department as well as citizen</p>
                             <p>Instant access of Vehicle/DL information to other government departments</p>
